d14fde369e0d1516f1f646859c928dc1ddd1b1b1
csleo95 <leonardo.saraiva@usp.br>
2023-05-29 18:40:15 -0300

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,9 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
+# This line specifies the master document.
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
